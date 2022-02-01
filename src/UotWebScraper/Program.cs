5742e79b22e73fca04821e01dbea496745e39cb8
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-02-01 14:12:40 +0200

    Add code to read from csv file

--- src/UotWebScraper/Program.cs
+++ src/UotWebScraper/Program.cs
@@ -1,24 +1,59 @@
-﻿using HtmlAgilityPack;
+﻿using CsvHelper;
+using HtmlAgilityPack;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 
 namespace UotWebScraper
 {
     public class Program
     {
-        private static string url = "url";
+        //private static string url;
 
         private static void Main()
         {
-            HtmlWeb htmlWeb = new HtmlWeb();
+            IEnumerable<GSProfile> urls;
 
-            HtmlDocument htmlDocument = htmlWeb.Load(url);
+            using (var streamReader = new StreamReader("urls.csv"))
+            {
+                using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+                {
+                    urls = csvReader.GetRecords<GSProfile>().ToList();
+                }
+            }
 
-            var nameAndSurname = htmlDocument.DocumentNode.SelectNodes("//div[@id='gsc_prf_in']");
+            foreach (var url in urls)
+            {
+                Console.WriteLine(url.Url);
+            }
 
-            Console.WriteLine(nameAndSurname[0].InnerText);
+            //HtmlWeb htmlWeb = new HtmlWeb();
+
+            //HtmlDocument htmlDocument = htmlWeb.Load(url);
+
+            //var nameAndSurname = htmlDocument.DocumentNode.SelectNodes("//div[@id='gsc_prf_in']");
+
+            //using (var streamWriter = new StreamWriter("test.csv"))
+            //{
+            //    using (var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
+            //    {
+            //        csvWriter.WriteField(nameAndSurname[0].InnerText);
+            //    }
+            //}
+
+            //Console.WriteLine(nameAndSurname[0].InnerText);
+
+            //Console.WriteLine("Success");
 
             Console.ReadKey(true);
         }
+    }
+
+    public class GSProfile
+    {
+        public string Url { get; set; }
     }
 
     public class User
@@ -32,5 +67,10 @@ namespace UotWebScraper
         public int HIndex { get; set; }
 
         public int I10Index { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name},{Surname}";
+        }
     }
 }
