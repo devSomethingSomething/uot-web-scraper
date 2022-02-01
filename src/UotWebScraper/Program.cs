28696b0cb19fec643f32408ad0e62c79027bae95
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-02-01 15:46:16 +0200

    Refactor code

--- src/UotWebScraper/Program.cs
+++ src/UotWebScraper/Program.cs
@@ -5,103 +5,81 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Threading;
 
 namespace UotWebScraper
 {
     public class Program
     {
-        private static void Main()
+        private static List<User> users = new List<User>();
+
+        private static List<Profile> profiles = new List<Profile>();
+
+        private const string PROFILES_FILENAME = "profiles.csv";
+
+        private const string USERS_FILENAME = "users.csv";
+
+        private static void GetProfiles()
         {
-            // Temp URL, should be read from a file
-            string url = "https://scholar.google.com/citations?user=1OyvitkAAAAJ&hl=en&oi=ao";
-
-            // Will hold a list of users with data from GS
-            var users = new List<User>();
-
-            //IEnumerable<GSProfile> urls;
-
-            //using (var streamReader = new StreamReader("urls.csv"))
-            //{
-            //    using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
-            //    {
-            //        urls = csvReader.GetRecords<GSProfile>().ToList();
-            //    }
-            //}
-
-            //foreach (var url in urls)
-            //{
-            //    Console.WriteLine(url.Url);
-            //}
-
-            HtmlWeb htmlWeb = new HtmlWeb();
-
-            HtmlDocument htmlDocument = htmlWeb.Load(url);
-
-            var nameAndSurname = htmlDocument.DocumentNode.SelectNodes("//div[@id='gsc_prf_in']");
-
-            var citations = htmlDocument.DocumentNode.SelectNodes("//table[@id='gsc_rsb_st']/tbody/tr[1]/td[2]");
-
-            var hIndex = htmlDocument.DocumentNode.SelectNodes("//table[@id='gsc_rsb_st']/tbody/tr[2]/td[2]");
-
-            var i10Index = htmlDocument.DocumentNode.SelectNodes("//table[@id='gsc_rsb_st']/tbody/tr[3]/td[2]");
-
-            var name = nameAndSurname[0].InnerText.Split(" ").First();
-
-            var surname = nameAndSurname[0].InnerText.Split(" ").Last();
-
-            users.Add(new User()
+            using (var streamReader = new StreamReader(PROFILES_FILENAME))
             {
-                Name = name,
-                Surname = surname,
-                Citations = Convert.ToInt32(citations[0].InnerText),
-                HIndex = Convert.ToInt32(hIndex[0].InnerText),
-                I10Index = Convert.ToInt32(i10Index[0].InnerText)
-            });
+                using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+                {
+                    profiles = csvReader.GetRecords<Profile>().ToList();
+                }
+            }
+        }
 
-            using (var streamWriter = new StreamWriter("users.csv"))
+        private static void GetUsers()
+        {
+            foreach (var profile in profiles)
+            {
+                HtmlWeb htmlWeb = new HtmlWeb();
+
+                HtmlDocument htmlDocument = htmlWeb.Load(profile.Url);
+
+                User user = new User();
+
+                user.Name = htmlDocument.DocumentNode.SelectNodes("//div[@id='gsc_prf_in']")[0].InnerText.Split(" ").First();
+
+                user.Surname = htmlDocument.DocumentNode.SelectNodes("//div[@id='gsc_prf_in']")[0].InnerText.Split(" ").Last();
+
+                user.Citations = Convert.ToInt32(htmlDocument.DocumentNode.SelectNodes("//table[@id='gsc_rsb_st']/tbody/tr[1]/td[2]")[0].InnerText);
+
+                user.HIndex = Convert.ToInt32(htmlDocument.DocumentNode.SelectNodes("//table[@id='gsc_rsb_st']/tbody/tr[2]/td[2]")[0].InnerText);
+
+                user.I10Index = Convert.ToInt32(htmlDocument.DocumentNode.SelectNodes("//table[@id='gsc_rsb_st']/tbody/tr[3]/td[2]")[0].InnerText);
+
+                users.Add(user);
+
+                Console.WriteLine(user.ToString());
+
+                Thread.Sleep(30000);
+            }
+        }
+
+        private static void WriteUsers()
+        {
+            using (var streamWriter = new StreamWriter(USERS_FILENAME))
             {
                 using (var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
                 {
-                    //csvWriter.WriteField(nameAndSurname[0].InnerText);
-
                     csvWriter.WriteRecords(users);
                 }
             }
+        }
 
-            Console.WriteLine(nameAndSurname[0].InnerText);
+        private static void Main()
+        {
+            GetProfiles();
 
-            Console.WriteLine(citations[0].InnerText);
+            GetUsers();
 
-            Console.WriteLine(hIndex[0].InnerText);
-
-            Console.WriteLine(i10Index[0].InnerText);
+            WriteUsers();
 
             Console.WriteLine("Success");
 
             Console.ReadKey(true);
         }
-    }
-
-    public class GSProfile
-    {
-        public string Url { get; set; }
-    }
-
-    public class User
-    {
-        public string Name { get; set; }
-
-        public string Surname { get; set; }
-
-        public int Citations { get; set; }
-
-        public int HIndex { get; set; }
-
-        public int I10Index { get; set; }
-
-        //public override string ToString()
-        //{
-        //    return $"{Name},{Surname}";
-        //}
     }
 }
---
+++ src/UotWebScraper/User.cs
@@ -0,0 +1,25 @@
+﻿namespace UotWebScraper
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Citations { get; set; }
+
+        public int HIndex { get; set; }
+
+        public int I10Index { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Name},{Surname},{Citations},{HIndex},{I10Index}";
+        }
+    }
+}
